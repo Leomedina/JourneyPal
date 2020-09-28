d40347687491ee61febdbf5b7ded12d08c22e306
Leo <medinayleo@gmail.com>
2020-09-28 15:35:26 -0400

    more boilerplate react aand start of form

---
+++ src/pages/Index/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import JourneyForm from '../../components/JourneyForm'
+
+function Home() {
+  return (
+    <>
+      <h1>Homepage</h1>
+      <JourneyForm />
+    </>
+  )
+
+};
+
+export default Home;
